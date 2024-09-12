e59dcc0ce3ea2d0a6ae29732826cec7030367fba
Jonathan <jonathanqiao1@gmail.com>
2024-09-12 14:18:11 -0400

    Fixed multiples class

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,5 +1,5 @@
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println(Math.floor(999 / 3) + Math.floor(999 / 5));
+        System.out.println(Math.floor(999 / 3) + Math.floor(999 / 5) - Math.floor(999 / 15));
     }
 }
